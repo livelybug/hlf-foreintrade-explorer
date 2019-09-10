f74897e9e3c5e973b18cdcbed20a85fdfc7951dc
burt <chensu@jinzhongkeji.com>
2019-09-10 14:32:32 +0800

    Loop the trade, avoid exit in 3 hours

--- hyperledger-fabric/scripts/script.sh
+++ hyperledger-fabric/scripts/script.sh
@@ -373,7 +373,7 @@ echo ---------------------------
 echo "Foreign Trade  End to End application completed."
 echo ---------------------------
 
-sleep 3600
+sleep 1h
 TRADE_ID="$(($TRADE_ID + 1))"
 
 #Reset chaincode state
